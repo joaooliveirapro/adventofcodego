ebf904605e89dae556daa7fb72efc062cd2da1f7
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2025-01-09 09:53:47 +0000

    Day 6

---
+++ 2015/day6/day6.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/joaooliveirapro/adventofcodego/utils"
+
+func part1(input *[]string) {
+
+}
+
+func main() {
+	input := utils.ReadInput("./2015/day6/day6.input.txt")
+	part1(&input)
+}
